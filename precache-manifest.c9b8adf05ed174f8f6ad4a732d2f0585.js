5b86fefb7cf1fe1fa017ac6b52a400c209b90676
smiljkanenadovic <smiljka.nenadovic.bg@gmail.com~>
2020-09-24 22:08:46 +0200

    Updates

---
+++ precache-manifest.c9b8adf05ed174f8f6ad4a732d2f0585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "319b961ede13ef47d723",
+    "url": "/final-project/static/css/main.b68c8d68.chunk.css"
+  },
+  {
+    "revision": "319b961ede13ef47d723",
+    "url": "/final-project/static/js/main.9896db6f.chunk.js"
+  },
+  {
+    "revision": "cabc802b7bb02805ec54",
+    "url": "/final-project/static/js/runtime~main.797a4eca.js"
+  },
+  {
+    "revision": "25510a8ecdb72fe4f52b",
+    "url": "/final-project/static/js/2.6ea34ac3.chunk.js"
+  },
+  {
+    "revision": "b86fce5aa8bb040f26f7cb8c30a139d4",
+    "url": "/final-project/static/media/836 [Converted].b86fce5a.svg"
+  },
+  {
+    "revision": "718e616c286a95d94195583e7ea62005",
+    "url": "/final-project/index.html"
+  }
+];
